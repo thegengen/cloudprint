0cbb5b9dedb6cce0a2accf6778a28c5646cf4147
Jim Ryan <jim@room118solutions.com>
2012-10-08 21:09:04 -0400

    Refactor PrintJob a bit

--- lib/cloudprint/print_job.rb
+++ lib/cloudprint/print_job.rb
@@ -6,18 +6,8 @@ module CloudPrint
       @data = data
     end
 
-    def self.find(jobid)
-      job = find_by_id(jobid)
-      return nil if job.nil?
-      new_from_response job
-    end
-
-    def self.all
-      fetch_jobs.map { |j| new_from_response j }
-    end
-
     def refresh!
-      @data = self.class._normalize_response_data(self.class.find_by_id(id))
+      @data = Util.normalize_response_data(self.class.find_by_id(id))
       self
     end
 
@@ -36,36 +26,50 @@ module CloudPrint
       end
     end
 
-  private
-
-    def self.find_by_id(id)
-      fetch_jobs.select{ |job| job['id'] == id }.first
+    module Util
+      def self.normalize_response_data(response_hash)
+        {
+          :id => response_hash['id'],
+          :status => response_hash['status'],
+          :error_code => response_hash['errorCode'],
+          :printer_id => response_hash['printerid'],
+          :title => response_hash['title'],
+          :content_type => response_hash['contentType'],
+          :file_url => response_hash['fileUrl'],
+          :ticket_url => response_hash['ticketUrl'],
+          :create_time => Time.at(response_hash['createTime'].to_f / 1000),
+          :update_time => Time.at(response_hash['updateTime'].to_f / 1000),
+          :message => response_hash['message'],
+          :tags => response_hash['tags']
+        }
+      end
     end
 
-    def self.fetch_jobs
-      response = CloudPrint.connection.get('/jobs') || {}
-      response['jobs'] || []
-    end
+    class << self
+      def find(jobid)
+        job = find_by_id(jobid)
+        return nil if job.nil?
+        new_from_response job
+      end
 
-    def self.new_from_response(response_hash)
-      new _normalize_response_data(response_hash)
-    end
+      def all
+        fetch_jobs.map { |j| new_from_response j }
+      end
 
-    def self._normalize_response_data(response_hash)
-      {
-        :id => response_hash['id'],
-        :status => response_hash['status'],
-        :error_code => response_hash['errorCode'],
-        :printer_id => response_hash['printerid'],
-        :title => response_hash['title'],
-        :content_type => response_hash['contentType'],
-        :file_url => response_hash['fileUrl'],
-        :ticket_url => response_hash['ticketUrl'],
-        :create_time => Time.at(response_hash['createTime'].to_f / 1000),
-        :update_time => Time.at(response_hash['updateTime'].to_f / 1000),
-        :message => response_hash['message'],
-        :tags => response_hash['tags']
-      }
+      private
+
+      def find_by_id(id)
+        fetch_jobs.select{ |job| job['id'] == id }.first
+      end
+
+      def fetch_jobs
+        response = CloudPrint.connection.get('/jobs') || {}
+        response['jobs'] || []
+      end
+
+      def new_from_response(response_hash)
+        new Util.normalize_response_data(response_hash)
+      end
     end
   end
 end
